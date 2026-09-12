5055e351586443dbe3045d33e8b27d39af1b5189
rohit <dhakarohit0@gmail.com>
2026-09-12 20:07:30 +0530

    Delete unwanted dir

--- modules_backup/azurerm_public_ip/main.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_public_ip" "public_ips" {
-  for_each            = var.pips
-  name                = each.value.pip_name
-  resource_group_name = each.value.pip_resource_group_name
-  location            = each.value.pip_location
-  allocation_method   = each.value.pip_allocation_method
-}
--- modules_backup/azurerm_subnet/main.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_subnet" "subnets" {
-  for_each             = var.subnets
-  name                 = each.value.snet_name
-  virtual_network_name = each.value.snet_virtual_network_name
-  resource_group_name  = each.value.snet_resource_group_name
-  address_prefixes     = each.value.snet_address_prefixes
-}
